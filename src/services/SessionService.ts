914a21466552fa29f595ceff6773d4c32ccce4c1
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2021-07-07 09:01:40 -0300

    service and user login controller

--- src/services/SessionService.ts
+++ src/services/SessionService.ts
@@ -1,5 +1,5 @@
 import { users } from '../entities/User';
-import { hash } from "bcryptjs";
+import { compare } from "bcryptjs";
 import { sign } from "jsonwebtoken";
 import { AppError } from 'src/errors/AppError';
 
@@ -10,8 +10,42 @@ interface IUserRequest {
 
 class SessionService {
   async create({ username, password }: IUserRequest) {
+    if (!username) {
+      throw new AppError("Username/Password incorrect!");
+    }
 
+    if (!password) {
+      throw new AppError("Username/Password incorrect!");
+    }
+
+    const user = await users.findOne({
+      username: username.trim()
+    });
+
+    if (!user) {
+      throw new AppError("Username/Password incorrect!");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new AppError("Username/Password incorrect!");
+    }
+
+    const token = sign({
+      username: user.username,
+      phone: user.phone,
+      isOnline: user.isOnline
+    }, process.env.TOKEN, {
+      subject: user.id,
+      expiresIn: "1d"
+    });
+
+    return token;
   }
 }
 
 export { SessionService }
+
+
+// TODO: colocar usuário online quando ele faz o login e criar rota de logout
