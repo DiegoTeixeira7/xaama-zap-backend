81267b688775200acd75ededa5ab857f220a9fbf
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2021-09-08 13:55:03 -0300

    reworked settings for class

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,23 +1,31 @@
 //	Loading mongoose and dotenv modules
 import mongoose from "mongoose";
 require("dotenv").config();
+class Database {
+  private uri: string;
 
-//	Setting up mongoose
-mongoose.Promise = global.Promise;
-mongoose.set("useFindAndModify", false);
+  //	Setting up database connection
+  constructor() {
+    mongoose.Promise = global.Promise;
+    mongoose.set("useFindAndModify", false);
 
-//	Defining database server
-const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.DBPASSWORD}@cluster0.1kiaj.mongodb.net/xaamazap?retryWrites=true&w=majority`;
+    //	Defining database server
+    this.uri = `mongodb+srv://${process.env.USERNAME}:${process.env.DBPASSWORD}@cluster0.1kiaj.mongodb.net/xaamazap?retryWrites=true&w=majority`;
+  }
 
-//	Connecting to database
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log("Connection to database has been established successfully.");
-}).catch((error) => {
-  console.error("Unable to connect to the database:\n", error);
-});
+  //	Connect to database
+  public async connect(): Promise<void> {
+    //	Connecting to database
+    mongoose.connect(this.uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }).then(() => {
+      console.log("Connection to database has been established successfully.");
+    }).catch((error) => {
+      console.error("Unable to connect to the database:\n", error);
+    });
+  }
+}
 
-//	Exporting database connection
-module.exports = mongoose;
+//	Exporting database object
+export default new Database();
